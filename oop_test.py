0e7dabc814aeab04328dd77c9ec4c165413b2d1f
builuk1 <builuk@itstep.academy>
2020-09-09 20:17:08 +0300

    My Django Blog

---
+++ oop_test.py
@@ -0,0 +1,11 @@
+# class Post:
+#     text = "text"
+#     id = "id"
+#     user = User()
+#     def __str__(self):
+#         return self.text
+#
+# p = Post()
+#
+# print(p)
+# pip install -r requirements.txt
